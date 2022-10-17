9c2fa3ce87f989e6a87c0c6af5274cbc7d99cefa
Mark Valentine Aikins <valentineaikins@icloud.com>
2022-10-17 12:22:46 +0000

    First program in Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Mark Valentine Aikins");
+        System.out.println("Bsc Business Information Technology");
+        System.out.println("maikins@palm.edu.gh");
+        System.out.print("Designation:Lecturer\n");
+        System.out.print("Server Information  ");
+    }
+}
